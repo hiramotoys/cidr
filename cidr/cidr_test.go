0898d28f62e20b6465aaf7a564dd7919a033c9e3
hiramotoys <hiramoto.ys@gmail.com>
2019-01-03 19:44:53 +0900

    Add a test case

--- cidr/cidr_test.go
+++ cidr/cidr_test.go
@@ -18,6 +18,13 @@ func TestNewCidrCase002(t *testing.T) {
 	checkCidr("Case001", cb.GetCidr(), "192.168.32.0/30", t)
 }
 
+func TestNewCidrCase003(t *testing.T) {
+	cb, _ := NewCidr("10.100.0.0/8")
+	checkNetworkAddress("Case003", cb.GetNetworkAddress(), "10.0.0.0", t)
+	checkBroadcastAddress("Case003", cb.GetBroadcastAddress(), "10.255.255.255", t)
+	checkCidr("Case003", cb.GetCidr(), "10.0.0.0/8", t)
+}
+
 func checkNetworkAddress(caseID, actualAddress, okAddress string, t *testing.T) {
 	if actualAddress != okAddress {
 		t.Errorf("%s case failed. Network address incorrectly. %s is correctly, but actual address is %s .\n", caseID, okAddress, actualAddress)
