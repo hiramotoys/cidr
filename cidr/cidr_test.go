cf1fe85ac2a8cfc4bd52d6d8ab53afdbe6a47f40
hiramotoys <hiramoto.ys@gmail.com>
2019-01-03 19:32:03 +0900

    Add test cases

--- cidr/cidr_test.go
+++ cidr/cidr_test.go
@@ -8,6 +8,14 @@ func TestNewCidrCase001(t *testing.T) {
 	cb, _ := NewCidr("192.168.32.0/28")
 	checkNetworkAddress("Case001", cb.GetNetworkAddress(), "192.168.32.0", t)
 	checkBroadcastAddress("Case001", cb.GetBroadcastAddress(), "192.168.32.15", t)
+	checkCidr("Case001", cb.GetCidr(), "192.168.32.0/28", t)
+}
+
+func TestNewCidrCase002(t *testing.T) {
+	cb, _ := NewCidr("192.168.32.0/30")
+	checkNetworkAddress("Case001", cb.GetNetworkAddress(), "192.168.32.0", t)
+	checkBroadcastAddress("Case001", cb.GetBroadcastAddress(), "192.168.32.3", t)
+	checkCidr("Case001", cb.GetCidr(), "192.168.32.0/30", t)
 }
 
 func checkNetworkAddress(caseID, actualAddress, okAddress string, t *testing.T) {
@@ -19,5 +27,11 @@ func checkNetworkAddress(caseID, actualAddress, okAddress string, t *testing.T) 
 func checkBroadcastAddress(caseID, actualAddress, okAddress string, t *testing.T) {
 	if actualAddress != okAddress {
 		t.Errorf("%s case failed. Broadcast address incorrectly. %s is correctly, but actual address is %s .\n", caseID, okAddress, actualAddress)
+	}
+}
+
+func checkCidr(caseID, actualCidr, okCidr string, t *testing.T) {
+	if actualCidr != okCidr {
+		t.Errorf("%s case failed. CIDR is incorrectly. %s is correctly, but actual address is %s .\n", caseID, okCidr, actualCidr)
 	}
 }
